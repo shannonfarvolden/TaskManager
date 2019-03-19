7d5d19e169b9cd208a9453415f541bf244f9b760
gloriading <gloria.ding@broadridge.com>
2019-03-19 06:52:02 -0700

    Move brand to separate component

--- client/components/MainLayout.js
+++ client/components/MainLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Layout, Menu, Icon } from 'antd';
 import '../components/styles/mainLayout.css';
+import Brand from './Brand';
 
 const { Content, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
@@ -23,9 +24,7 @@ class SiderNav extends React.Component {
         <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
           <div className="logo" />
           <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-            <div className="brand">
-              <span className="brand-drf">DRF</span> <span style={{ display: collapsed ? 'none' : 'inline-block' }}>TRACKER</span>
-            </div>
+            <Brand collapsed={collapsed} />
             <Menu.Item key="1" onClick={handleMenuSelection}>
               <Icon type="table" />
               <span>Summary</span>
