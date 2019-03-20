335293bfced6ff51f31cb9c0576d7c2698abe3dd
gloriading <gloria.ding@broadridge.com>
2019-03-20 09:30:34 -0700

    remove pills and searchArea from index.js

--- client/components/index.js
+++ client/components/index.js
@@ -1,7 +1,5 @@
 export { default as MainHeader } from './MainHeader';
 export { default as DatesChart } from './DatesChart';
 export { default as MainLayout } from './MainLayout';
-export { default as Pills } from './Pills';
-export { default as SearchArea } from './SearchArea';
 export { default as SummaryTable } from './SummaryTable';
 export { default as SummaryPage } from './SummaryPage';
