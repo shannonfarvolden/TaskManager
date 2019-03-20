93bfd98cb17a9ae4edb85ef0d1cf3a6c00468c0f
gloriading <gloria.ding@broadridge.com>
2019-03-20 08:02:25 -0700

    remove SearchArea

--- client/components/SummaryPage.js
+++ client/components/SummaryPage.js
@@ -1,11 +1,9 @@
 import { Layout } from 'antd';
 const { Content } = Layout;
-import SearchArea from './SearchArea';
 import SummaryTable from './SummaryTable';
 
 const SummaryPage = () => (
   <Content>
-    <SearchArea />
     <SummaryTable />
   </Content>
 );
